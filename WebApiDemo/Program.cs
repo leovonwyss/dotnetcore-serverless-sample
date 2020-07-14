5c6109eb514d929c393b3f56ab13c0ef63f708db
von Wyss, Leo <leo.vonwyss@zuehlke.com>
2020-07-14 09:20:22 +0200

    Added Lambda Runtime elements

--- WebApiDemo/Program.cs
+++ WebApiDemo/Program.cs
@@ -1,26 +1,60 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Amazon.Lambda.ApplicationLoadBalancerEvents;
+using Amazon.Lambda.AspNetCoreServer;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.Json;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace WebApiDemo
 {
     public class Program
     {
+        private class LambdaEntryPoint :
+            ApplicationLoadBalancerFunction
+        {
+            protected override void Init(IWebHostBuilder builder)
+            {
+                ConfigureWebHostBuilder(builder);
+            }
+        }
+        
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            if (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("AWS_LAMBDA_FUNCTION_NAME")))
+            {
+                RunServerless();
+            }
+            else
+            {
+                RunAsWebserver(args);
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static void RunServerless()
+        {
+            var lambdaEntry = new LambdaEntryPoint();
+            Func<ApplicationLoadBalancerRequest, ILambdaContext, Task<ApplicationLoadBalancerResponse>> functionHandler =
+                lambdaEntry.FunctionHandlerAsync;
+            using var handlerWrapper = HandlerWrapper.GetHandlerWrapper(functionHandler, new JsonSerializer());
+            using var bootstrap = new LambdaBootstrap(handlerWrapper);
+            bootstrap.RunAsync().Wait();
+        }
+
+        private static void RunAsWebserver(string[] args)
+        {
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(ConfigureWebHostBuilder)
+                .Build()
+                .Run();
+        }
+
+        public static void ConfigureWebHostBuilder(IWebHostBuilder webHostBuilder)
+        {
+            webHostBuilder
+                .UseStartup<Startup>();
+        }
     }
 }
